78c9a121d5fa51ad7178a11d806c95921525ad73
JoJo <jhkaler42@gmail.com>
2025-05-01 06:33:50 -0400

    i think done

--- unity_swen711/Assets/scipts/Robot/RobotController.cs
+++ unity_swen711/Assets/scipts/Robot/RobotController.cs
@@ -221,13 +221,11 @@ public class RobotController : MonoBehaviour
                 break;
                 
             case RobotMode.Stealth:
-                moveSpeed = baseSpeed * 0.8f;
                 batteryDrainRate = 1.2f;
                 if (weaponSystem != null) weaponSystem.damageMultiplier = stealthDamageMultiplier;
                 break;
                 
             case RobotMode.Combat:
-                moveSpeed = baseSpeed * 0.7f;
                 batteryDrainRate = 1.5f;
                 if (weaponSystem != null) weaponSystem.damageMultiplier = combatDamageMultiplier;
                 if (damageSystem != null) damageSystem.damageReduction = combatDamageReduction;
